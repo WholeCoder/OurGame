96331209713c54a3a2e29c3f737d9df25a8aa2d0
WholeCoder <rpierich@hotmail.com>
2014-12-18 20:25:39 -0500

    Organized using statements.

--- WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -1,17 +1,13 @@
-using System.Text;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using System.Xml.Serialization;
+using System.Text;
+using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Graphics;
+
+// My usings.
 using Command;
 
 namespace WindowsGame1
