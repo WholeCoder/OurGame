4af85798451477a8ca0dac33381e7e7b3b758533
WholeCoder <rpierich@hotmail.com>
2014-12-21 14:14:03 -0500

    Prettied up the usings.

--- WindowsGame1/Command/PlaceTileOnBoardCommand.cs
+++ WindowsGame1/Command/PlaceTileOnBoardCommand.cs
@@ -2,12 +2,12 @@
 using System.Linq;
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.GamerServices;
 
 using WindowsGameLibrary1;
 
