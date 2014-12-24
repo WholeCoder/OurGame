5d73ce8de7295e1ae10388964638d5c46c874857
WholeCoder <rpierich@hotmail.com>
2014-12-24 17:34:40 -0500

    Updated some comments.

--- WindowsGame1/GameState/GameCircularDependencyFixInterface.cs
+++ WindowsGame1/GameState/GameCircularDependencyFixInterface.cs
@@ -6,15 +6,17 @@ using System.Collections.Generic;
 
 namespace GameState
 {
-    // This interface is used to avoid a circular reference.  It is applied to the Game1 class.
-    public interface GameCircularDependencyFixInterface
+    // This interface is used to avoid a circular reference between the Game1 class and State classes.  It is applied to the Game1 class.
+    public interface GameCircularDependencyFixInterface // for Game1 class!
     {
+        // These next methods are used by the State classes to switch between States.
         void setStateWhenUpdating(State state, GameTime gameTime);
         void setStateWhenInitializing(State state);
 
+        // Add a new one of these for each new state in the Game1 class.
         State getBlankState();
         State getEditBoardState();
 
-        void Exit2();
+        void Exit2(); // Alias for the Exit() method that quits the application altogether.
     }
 }
