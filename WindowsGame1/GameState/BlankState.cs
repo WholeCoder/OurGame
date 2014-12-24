c8eaace4a60160b15a378f102f198b5d4a18695b
WholeCoder <rpierich@hotmail.com>
2014-12-24 18:44:56 -0500

    Made B transition to blank state and E transition to Editor state.

--- WindowsGame1/GameState/BlankState.cs
+++ WindowsGame1/GameState/BlankState.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
 
 namespace GameState
 {
     // This class doesn't do anything. It is just used to demonstrate setStateWhenUpdating() and setStateWhenInitializing().
     public class BlankState : State
     {
+        KeyboardState oldKeyboardState;
+
+        // Call setStateWhenUpdating on this instance variable to change to a different game state.
+        public GameCircularDependencyFixInterface OurGame { get; set; }
+
         public BlankState()
         {
             
@@ -15,7 +21,7 @@ namespace GameState
 
         public override void Initialize(GameCircularDependencyFixInterface ourGame)
         {
-
+            this.OurGame = ourGame;
         }
 
         public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager Content)
@@ -30,7 +36,14 @@ namespace GameState
 
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
+            KeyboardState newKeyboardState = Keyboard.GetState();  // get the newest state
 
+            if (newKeyboardState.IsKeyDown(Keys.E) && oldKeyboardState.IsKeyUp(Keys.E))
+            {
+                this.OurGame.setStateWhenUpdating(this.OurGame.getEditBoardState(), gameTime);
+            }
+
+            oldKeyboardState = newKeyboardState;  // set the new state as the old state for next time
         }
 
         public override void Draw(Microsoft.Xna.Framework.GameTime gameTime, Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
