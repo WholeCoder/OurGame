890385a254cedf897ecccb707f4cb6d64194b609
WholeCoder <rpierich@hotmail.com>
2014-12-29 16:48:30 -0500

    Removed unsued using statements in AnimatedSprite class.

--- WindowsGame1/WindowsGame1/WindowsGame1/Sprite/AnimatedSprite.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Sprite/AnimatedSprite.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 // My usings.
