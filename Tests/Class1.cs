b337b9b96d1fca3ffd1e7cd714d1711c258cf007
WholeCoder <rpierich@hotmail.com>
2015-01-16 20:26:57 -0500

    Updated for some reason.

--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -13,7 +13,7 @@ namespace Tests
         [Fact]
         public void FailingTest()
         {
-            Assert.Equal(4, Add(2, 2));
+            Assert.Equal(5, Add(2, 2));
         }
 
         int Add(int x, int y)
