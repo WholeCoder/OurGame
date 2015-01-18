ac8dcbb2c2f99a2992847e9c3d96c57d22ffd194
WholeCoder <rpierich@hotmail.com>
2015-01-18 14:54:31 -0500

    Fixed up the SpriteManager class.

--- OurGame/Sprites/SpriteManager.cs
+++ OurGame/Sprites/SpriteManager.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using System.Diagnostics;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 // My usings.
 using OurGame.GameStates;
@@ -15,21 +15,19 @@ namespace OurGame.Sprites
         public AnimatedSprite[] Sprites;
         public string SpritesFileName;
 
-        public SpriteManager(String spritesFileName, ContentManager Content, Board board, PlayGameState pState)
+        public SpriteManager(String spritesFileName, Board board, PlayGameState pState)
         {
             Debug.Assert(spritesFileName != null && !spritesFileName.Equals(""),"spritesFileName can not be null or empty!");
-            Debug.Assert(Content != null, "Content can not be null!");
             Debug.Assert(board != null, "board can not be null!");
             Debug.Assert(pState != null, "pState can not be null!");
 
             this.SpritesFileName = spritesFileName;
 
-            this.LoadSpritesFromAfile(Content, board, pState);
+            this.LoadSpritesFromAfile(board, pState);
         }
 
-        private void LoadSpritesFromAfile(ContentManager Content, Board board, PlayGameState pState)
+        private void LoadSpritesFromAfile(Board board, PlayGameState pState)
         {
-            Debug.Assert(Content != null, "Content can not be null!");
             Debug.Assert(board!= null, "board can not be null!");
             Debug.Assert(pState != null, "pState can not be null!");
 
@@ -43,7 +41,22 @@ namespace OurGame.Sprites
                 string currentSpriteFileName = configStringSplitRay[i + 1];
                 this.Sprites[i] = SimpleAnimatedSpriteFactory.CreateAnimatedSprite(currentSpriteFileName, board, pState);
             } // end for
-
         } // end method
+
+        public void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            for (int i = 0; i < this.Sprites.Length; i++)
+            {
+                this.Sprites[i].Update(gameTime);
+            } // end for
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < this.Sprites.Length; i++)
+            {
+                this.Sprites[i].Draw(spriteBatch);
+            } // end for
+        }
     } // end class
 } // end using
