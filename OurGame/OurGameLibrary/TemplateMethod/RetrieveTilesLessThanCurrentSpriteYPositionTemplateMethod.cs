ae8b4404df4d9e0f53d40d275bf986a12f26287d
WholeCoder <rpierich@hotmail.com>
2015-02-11 15:56:31 -0500

    Removed unused using directives.

--- OurGame/OurGameLibrary/TemplateMethod/RetrieveTilesLessThanCurrentSpriteYPositionTemplateMethod.cs
+++ OurGame/OurGameLibrary/TemplateMethod/RetrieveTilesLessThanCurrentSpriteYPositionTemplateMethod.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace OurGame.OurGameLibrary.TemplateMethod
+﻿namespace OurGame.OurGameLibrary.TemplateMethod
 {
     class RetrieveTilesLessThanCurrentSpriteYPositionTemplateMethod : RetrieveTilesTemplateMethod
     {
